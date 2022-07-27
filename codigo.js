10ee192f66b8c0f50696fad26a3d5caa54cc41f1
Juampa <juanpguirao@gmail.com>
2022-07-26 23:54:01 -0300

    agregando productos

--- codigo.js
+++ codigo.js
@@ -1,32 +1,28 @@
 
-
 main();
 function main(){
-    let producto = parseInt( prompt("Elija el tipo de producto:\n1)invierno\n2)verano") );
+    let producto = parseInt( prompt("Elija el tipo de producto:\n1)Funcional \n2)Maquinas") );
 
     if( producto == 1)
     {
-        //muestra invierno
-        mostrar();
+        //opcion iniciante
+        let ticket1 = "Dos clase $2000\nTres Clases $2500\nCuatro Clases $2800\nCinco Clases $3200"
+        mostrar(ticket1);
     }
     else if( producto == 2)
     {
         //verano
-        mostrar();
+        let ticket2 = "Dos dias $1500\nTres dias $1800\nCuatro dias $2100\nCinco dias $2500"
+        mostrar(ticket2);
     }
     else {
         alert("El valor  ingresado no es valido, por favor ingrese una nueva opcion")
-        main();
+        main(); //vuelve a llamar en caso de ingresar informacion incorrecta
     }
 }
 
 
-function mostrar()
+function mostrar(catalogo)
 {
-    console.log ("Campera\nGuantes\nSweater\nBufanda\nChaleco");
-}
-
-function mostrar()
-{
-    console.log ("Ojotas\nMalla\nSweater\nBufanda\nChaleco");
+    console.log (catalogo);
 }
