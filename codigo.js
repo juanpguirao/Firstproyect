0a5a70ffdeeb5e5e0033e1c8ea0f39d006c536db
Juampa <juanpguirao@gmail.com>
2022-08-04 23:24:25 -0300

    Subiendo arrays

--- codigo.js
+++ codigo.js
@@ -1,28 +1,74 @@
-
-main();
-function main(){
-    let producto = parseInt( prompt("Elija el tipo de producto:\n1)Funcional \n2)Maquinas") );
-
-    if( producto == 1)
-    {
-        //opcion iniciante
-        let ticket1 = "Dos clase $2000\nTres Clases $2500\nCuatro Clases $2800\nCinco Clases $3200"
-        mostrar(ticket1);
-    }
-    else if( producto == 2)
-    {
-        //verano
-        let ticket2 = "Dos dias $1500\nTres dias $1800\nCuatro dias $2100\nCinco dias $2500"
-        mostrar(ticket2);
-    }
-    else {
-        alert("El valor  ingresado no es valido, por favor ingrese una nueva opcion")
-        main(); //vuelve a llamar en caso de ingresar informacion incorrecta
+//se crean los arrays con los diferentes campos necesarios
+//Arrays de Usuarios registrados, con nombre apellido y clases a inscribirse
+class Usuario {
+    constructor(nombre, apellido, clase){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.clase=clase;
     }
 }
-
-
-function mostrar(catalogo)
-{
-    console.log (catalogo);
+//Arrays de Clases disponibles a inscribirse junto a un contador de alumnos inscriptos
+class Clase {
+    constructor(nombre, fechaInicio){
+        this.nombre=nombre;
+        this.fechaInicio=fechaInicio;
+        this.cantidadUsuarios=0;
+    }
 }
+ //Encontrar cuantos usuarios tiene cada clase
+function encontrarIndex(arrayClases, claseAEncontrar){
+    for (clase of arrayClases){
+        if(claseAEncontrar == clase.nombre)
+            return arrayClases.indexOf(clase);
+    }
+}
+//Muestra  los  usuario inscriptos en todas las clases
+function imprimirUsuarios(arrayUsuarios){
+    for(usuario of arrayUsuarios)
+            console.table(usuario)
+        }
+    
+// clases disponibles y fechas de inicio de cada clase
+const clases=[];
+clases.push(new Clase("Funcional", "1/01"));
+clases.push(new Clase("Natacion", "1/01"));
+clases.push(new Clase("Pilates", "1/01"));
+
+//Agrega nuevos usuarios al Arrays
+const usuarios=[];
+clases.push(new Usuario("Juan", "Guirao", "Funcional"));
+
+function ingresarUsuarios() {
+    let nombre;
+    do{
+        nombre =prompt("ingrese el nombre de Usuario");
+        if (nombre==""){
+            break;
+        }
+        let apellido =prompt("ingrese Apellido");
+        let clase =prompt("ingrese Clase a realizar");
+        let usuario = new Usuario(nombre, apellido, clase);
+
+        let indice = clases.find((clase)=>clase.nombre == usuario.clase)
+            
+        if (indice){
+                if(indice.cantidadUsuarios < 10){
+                    usuarios.push(usuario)
+                    let index = encontrarIndex(clases, clase)
+                    clases[index].cantidadUsuarios +=1;
+                    
+                    alert("la fecha de inicio es " +
+                    clases[index].fechaInicio +
+                    " y la cantidad de inscriptos es " +
+                    clases[index].cantidadUsuarios);
+                }
+                else{alert("los cupos estan llenos")
+            }
+            
+        }else{
+            alert ("La clase no existe")
+        }       
+     } while (nombre !="");
+}
+ingresarUsuarios();
+imprimirUsuarios(usuarios);
